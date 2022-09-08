4a024e83a0216fda103b40c213db5f1cd9ebe315
Michelle-Wanderi <michellewangari12@gmail.com>
2022-09-08 12:51:23 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Description: main - Prints out a string to stdoui.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

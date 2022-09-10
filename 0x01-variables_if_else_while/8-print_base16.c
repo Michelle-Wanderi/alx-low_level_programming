24d16e7c84a2ed0cdfd1ee366c5f48cb96fedb6b
Michelle-Wanderi <michellewangari12@gmail.com>
2022-09-09 23:14:51 -0700

    a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,23 @@
+
+
 #include <stdio.h>
 
 /**
- *  main - Prints a serie of numbers with commas
- *  Return: Always (Success);
+ * main - Prints a hexadecimal string
+ * Return: Always (Success)
  */
 int main(void)
 {
-	int c;
+	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
+	}
 
-		if (c != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
 	}
 
 	putchar('\n');

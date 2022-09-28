b654feaf3dcca374f9254c1f9ed03564d16f8ee4
Michelle-Wanderi <michellewangari12@gmail.com>
2022-09-28 12:42:29 -0700

    a function that returns the factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,15 +5,16 @@
  */
 
 int factorial(int n);
+int factorial(int n)
 {
-	if (n < 0)
-	return -1;
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
 
-	return * factorial ( n - 1);
-}
-
-	int main()
-{
-	printf("%d \n", factorial(n));
-	return 0;
+	return (n * factorial(n - 1));
 }
